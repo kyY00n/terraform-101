2c3893623005aaae0d31fb36d63df6cf2b2a0700
Kayoung Yoon <dev.kayoung@gmail.com>
2023-05-01 12:28:58 +0900

    ch8. vpc and subnet

---
+++ ch8/vpc.tf
@@ -0,0 +1,32 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform-101"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "terraform-101-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.10.0/24"
+  tags = {
+    Name = "terraform-101-private-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "terraform-101-igw"
+  }
+}
