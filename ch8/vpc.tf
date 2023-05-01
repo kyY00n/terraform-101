fc0c5b8ed4e232b2e29b6724763aec4caed7a5b2
Rosie <dev.kayoung@gmail.com>
2023-05-01 16:48:07 +0900

    ch9. igw와 nat gateway 생성하기

--- ch8/vpc.tf
+++ ch8/vpc.tf
@@ -30,3 +30,19 @@ resource "aws_internet_gateway" "igw" {
     Name = "terraform-101-igw"
   }
 }
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat.id
+  subnet_id = aws_subnet.public_subnet.id
+  tags = {
+    Name = "terraform-101-nat-gw"
+  }
+}
