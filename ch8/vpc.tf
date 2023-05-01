a303cbc1071e37506aeef203fe4120d3cf0de376
Rosie <dev.kayoung@gmail.com>
2023-05-01 17:17:34 +0900

    route table association 생성

--- ch8/vpc.tf
+++ ch8/vpc.tf
@@ -55,10 +55,20 @@ resource "aws_route_table" "public" {
   }
 }
 
+resource "aws_route_table_association" "route_table_association_public" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "terraform-101-rt-private"
   }
+}
+
+resource "aws_route_table_association" "route_table_association_private" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.private.id
 }
