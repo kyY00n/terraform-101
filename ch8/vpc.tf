2a130276991ab372896bb060db7f6973ffd8346c
Rosie <dev.kayoung@gmail.com>
2023-05-01 17:14:28 +0900

    route table 정의

--- ch8/vpc.tf
+++ ch8/vpc.tf
@@ -46,3 +46,19 @@ resource "aws_nat_gateway" "nat_gateway" {
     Name = "terraform-101-nat-gw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  
+  tags = {
+    Name = "terraform-101-rt-public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-101-rt-private"
+  }
+}
